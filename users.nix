63097f0a203a180cd08a75cc235a465317167901
Alex Shand <alexshand4827@gmail.com>
2017-12-16 16:16:21 +0000

    Testing filter

---
+++ users.nix
@@ -0,0 +1,38 @@
+{ config, pkgs, ... }:
+
+{
+
+  users = {
+
+    users.alex = {
+
+      # Creates a home directory at "/home/alex", sets group to users and default
+      # shell to users.defaultUserShell (Seems to be bash by default)
+      isNormalUser = true;
+
+      extraGroups = [
+        
+        # Use sudo 
+        "wheel"
+
+        # Control the network with networkmanager
+        "networkmanager"
+
+        # Enable interaction with libvirt
+        "libvirtd"
+
+      ];
+
+      # Default password is 'q' create a new one with mkpasswd -m sha-512 after
+      # install
+      hashedPassword = "TEST";
+      
+    };
+
+    # Users can only be added and modified by changing the configuration files
+    # (As a result there's currently no root user)
+    mutableUsers = false;
+
+  };
+  
+}
