14a901c88569e1cfe9f2d3ff658f9260b2f6fdcc
Alex Shand <alexshand4827@gmail.com>
2018-04-14 09:47:10 +0100

    14-05-18

--- network.nix
+++ network.nix
@@ -14,7 +14,13 @@ in
     hostName = host;
 
     # Control the network from the Desktop Manager
-    networkmanager.enable = true;
+    networkmanager = {
+      enable = true;
+      insertNameservers = [ "1.1.1.1" "1.0.0.1" ];
+    };
+
+    # DNS
+    #nameservers = [ "1.1.1.1" "1.0.0.1" ];
 
   };
   
